1957d43b0d7ed09c382c488bbb506173dc8cd369
Arian van Putten <aeroboy94@gmail.com>
2014-06-04 19:32:17 +0200

    Indent using 2 spaces.!

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ mongoose.connect(config.mongodb.url);
 
 /// configure translations
 i18n.configure({
-    locales: ['en', 'nl'],
-    directory: path.join(__dirname, 'locales'),
-    defaultLocale: 'en',
-    cookie: 'locale',
-    indent: '    '
+  locales: ['en', 'nl'],
+  directory: path.join(__dirname, 'locales'),
+  defaultLocale: 'en',
+  cookie: 'locale',
+  indent: '  '
 });
 
 
@@ -50,9 +50,9 @@ app.use('/', auth);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -60,23 +60,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
