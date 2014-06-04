c070380fc32e662d52e43d01331f32048536047c
Arian van Putten <aeroboy94@gmail.com>
2014-06-04 19:45:19 +0200

    Add login logic.

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../model/User');
+
+
 var router = express.Router();
+
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 router.get('/login', function (req,res) {
   res.render('login');
 });
 
-router.post('/login', function (req, res) {
-  // TODO login logic.
+router.post('/login', function (req, res, next) {
+  passport.authenticate('local', {
+    succesRedirect: req.session.lastPage || '/',
+    failureRedirect: '/login',
+    failureFlash: 'Invalid username or password.'
+  })(req,res,next);
 });
 
 router.get('/signup', function (req, res) {
@@ -14,8 +29,17 @@ router.get('/signup', function (req, res) {
 });
 
 router.post('/signup', function (req, res) {
-  // TODO signup logic.
-  );
+  User.register(new User({ email: req.body.email }), req.body.password, function (err, user) {
+    if (err) {
+      req.flash('error', err);
+      return res.redirect('/register');
+    }
+    req.login(user, function (err) {
+      // TODO not sure if I should ignore this error or not.
+      return res.redirect(req.session.lastPage || '/');
+    });
+  });
+});
 
 
 /// forgot password
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var i18n = require('i18n');
 var router = express.Router();
 
+function auth(req, res, next) {
+  if (!req.user) {
+    req.session.lastPage = req.path;
+    req.flash('info', i18n.__('You need to login to visit %s', req.path));
+    return res.redirect('/login');
+  }
+}
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
