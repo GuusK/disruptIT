b3dcdd84a0135ae8b9a0bce8fcf5f48394348187
Arian van Putten <aeroboy94@gmail.com>
2014-09-01 14:05:53 +0200

    Default the locale to Dutch for now untill we finish translations.

--- locale.js
+++ locale.js
@@ -3,6 +3,9 @@ var i18n = require('i18n');
 
 module.exports = function (req, res, next) {
   res.locals.locales = Object.keys(i18n.getCatalog());
+  if (!req.query.locale && !req.cookies.locale) {
+    req.query.locale = 'nl';
+  }
   if (req.query.locale) {
     res.cookie('locale', req.query.locale);
     res.setLocale(req.query.locale);
