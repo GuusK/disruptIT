776b2af461223ae8f532f0716065eff33389682b
Dennis Collaris <d.collaris@me.com>
2015-10-01 22:35:09 +0200

    initial work on speakers

--- generate-tickets.js
+++ generate-tickets.js
@@ -8,7 +8,7 @@ var config = JSON.parse(fs.readFileSync('config.json'));
 mongoose.connect(config.mongodb.url);
 
 function genTicket(cb) {
- return new Ticket({rev:1}).save(cb);
+ return new Ticket({rev:2}).save(cb);
 }
 var tasks = [];
 
