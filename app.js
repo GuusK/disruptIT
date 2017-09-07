39a12a711b1d722cd3c841aa2a1dbcea5ecc280d
G. Klinkenberg <g.klinkenberg@gmail.com>
2017-09-07 14:49:51 +0200

    Martijn fixed responsiveness of sponsorlogos

--- app.js
+++ app.js
@@ -119,15 +119,15 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // production error handler
 // no stacktraces leaked to user
