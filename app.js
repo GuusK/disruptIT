b4fa291b54a59e11f5e2e82775e1aad2256c305e
G. Klinkenberg <g.klinkenberg@gmail.com>
2017-09-07 13:47:10 +0200

    Started to make it production ready

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.use('/', auth);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Page not found');
   err.status = 404;
   next(err);
 });
@@ -119,19 +119,20 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  debug(err.status)
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
