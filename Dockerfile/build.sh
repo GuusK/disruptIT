c438507c7097557f2600ba253a5ec61cb6e9e84c
G. Klinkenberg <g.klinkenberg@gmail.com>
2017-09-29 13:51:05 +0000

    Updated proxy to redirect www to non-www and changed build of docker containers

--- Dockerfile/build.sh
+++ Dockerfile/build.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-ALL=false
-
-if [[ $1 == "all" ]] ; then
-	ALL=true
-fi
-
-if [[ $1 == "app" ]] || [[ ALL ]] ; then
-	docker build -t snic/disruptit:0.0.1 -f app.Dockerfile .
-fi
-
-if [[ $1 == "haproxy" ]] || [[ ALL ]] ; then
-	docker build -t snic/haproxy:0.0.1 -f haproxy.Dockerfile .
-fi
+docker build -t snic/disruptit:0.0.1 -f app.Dockerfile .
+docker build -t snic/haproxy:0.0.1 -f haproxy.Dockerfile .
