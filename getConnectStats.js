87d1145b3236f20a590b256e9dd33d71e566c193
G. Klinkenberg <g.klinkenberg@gmail.com>
2017-11-20 19:52:29 +0000

    Changes made on live

--- getConnectStats.js
+++ getConnectStats.js
@@ -7,12 +7,16 @@ var User     = require('./models/User');
 mongoose.connect(config.mongodb.url);
 
 var connectCount = 0;
+var participants = 0;
 
 User.find({}).exec( function (err, results) {
   if (err) { return err; }
   results.forEach(user=>{
     connectCount += user.connectlist.length;
+    if (user.connectlist.length > 0){
+        participants += 1;
+    }
   });
 }).then(nothing => {
-  console.log(connectCount);
+  console.log(participants + ' people made ' + connectCount + ' connections');
 });
